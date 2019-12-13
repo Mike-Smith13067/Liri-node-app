f590b2e28212f965a20d66652726955564604bb3
Mike Smith <hockeydad66@live.com>
2019-12-12 20:55:48 -0700

    argghh

--- liri-node-app.js
+++ liri-node-app.js
@@ -2,11 +2,7 @@ require("dotenv").config();
 const axios = require('axios');
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
-var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+const moment = require('moment');
 
 var command = process.argv[2];
 var search = process.argv.slice(3).join(" ");
@@ -37,46 +33,80 @@ var startProgram = function (command, search) {
             name: "name",
             message: "Please enter your name:",
         }
-    ]).then
-    })
-    switch (command) {
-        case "concert-this": getBands(search);
-            break;
-        case "spotify-this-song": getSpotify(search);
-            break;
-        case "movie-this": getMovie(search);
-            break;
-        case "do-what-it-says": doWhatItSays();
-            break;
-        default: console.log("I don't know what you are talking about");
-    }
-}
+        ]).then(function (input) {
+            logText(input.name);
+            inquirer.prompt([{
+                type: "confirm",
+                name: "choice",
+                message: "Hello " + input.name + "! Type your choise 'concert-this', 'movie-this', 'spotify-this-song', and the appropriate title"
 
-startProgram(command, search);
+            }
+            ]).then(function choice(input) {
+                logText(input.choice);
+                var userChoice = input.command;
+                var userArray = userChoice.split(" ");
+                var selection = userArray[0];
+                var search = userArray.slice(1).join("+");
+                if (choice === "movie-this") {
+                    axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy")
+                        .then(function (response) {
+                            console.log("\n***********Movie***********\n\n");
+                            console.log("Title: " + response.data.Title + "\nYear: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors and Actresses: " + response.data.Actors);
+                            return getChoice();
+                        });
+                } else if (choice === "concert-this") {
+                    axios.get("https://rest.bandsintown.co/artists/" + search + "/events?app_id=codingbootcamp")
+                        .then(function (response) {
+                            console.log("\n***********Concerts***********\n\n");
+                            console.log("\nVenue Name: " + events[i], venue.name + "\nLocation: " + events[i].venue.city + "," + events[i].venue.country + "\nDate: " + SVGAnimateMotionElement(events[i].venuew.datetime).format("MM/DD/YYYY"));
+                            return getChoice();
+                        });
+                } else {
+                    console.log("****************");
+                    console.log("No upcoming events for your choice.");
+                    return getChoice();
+                }
+            });
 
-function getSpotify(songName) {
-    if (songName == undefined) {
-        songName = "Amarillo By Morning"
-    }
-    spotify.search(
-        {
-            type: 'track',
-            query: songName,
-            limit: 5
-        },
-        function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            var songs = data.tracks.items
-            //console.log(JSON.stringify(songs))
-            for (var i = 0; i < songs.length; i++) {
-                console.log("-----------------");
-                console.log("artist name:" + songs[i].artists[0].name);
-                console.log("song name: " + songs[i].name);
-                console.log("preview link:" + songs[i].preview_url);
-                console.log("album: " + songs[i].album.name);
-            }
+        }else if (choice === "spotify-this-song") {
+            spotify.search({
+                type: "track",
+                query: search,
+                limit: 1
+
+            }, function (err, data) {
+                if (err) {
+                    console.log("Error:" + err);
+                    return getChoice();
+                } else {
+                    var songTitle = data.tracks.items
+                    for (var i = 0; i < songTitle.length; i++) {
+                        console.log("***************Song************");
+                        var songData = song[i].artists;
+                        for (varj = 0; j < songData.length; j++); {
+                            if (songData.length > 1) {
+                                var songList = songData[j].name;
+                                console.log(songList);
+                            } else {
+                                console.log(songData[j].name);
+                            }
+                        }
+                        console.log("Title: " + song[i].name + "\nLink: " + song[i].preview_url + "\nAlbum: " + song[i].album.name);
+                        return getChoice();
+                    }
+                }
+            })
+        } else if (choice === "do-what-it-says") {
+            fs.readFile("random.txt", "utf8", function (error, data) {
+                var dataArray = data.split(",".join(" ");
+                if (error) {
+                    console.log(error);
+                    return getChoice();
+                }else if (command === ""){
+                    return;
+                }else {
+                    console.log ("Invalid command= Try again");
+                    return getChoice();
+                }
         });
-
 }
