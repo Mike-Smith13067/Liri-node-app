55a5b16dedf333b954ce629d5f0a2e125b7e03a8
Mike Smith <hockeydad66@live.com>
2019-12-12 19:28:34 -0700

    adding a password and user name function

--- liri-node-app.js
+++ liri-node-app.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const axios = require('axios');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -12,10 +16,33 @@ console.log("search term :" + search)
 
 
 var startProgram = function (command, search) {
+    inquirer.prompt([{
+        type: "password",
+        name: "password",
+        message: "Please enter the password. (*LIRI*)",
+        validate: function (input) {
+            if (input === "LIRI") {
+                return true;
+            } else if (input === "") {
+                return "Please enter the password (LIRI)"
+            } else {
+                return "Wrong- Try again!"
+            }
+        }
+    }
+    ]).then(function (input) {
+        logText(input.password);
+        inquirer.prompt([{
+            type: "input",
+            name: "name",
+            message: "Please enter your name:",
+        }
+    ]).then
+    })
     switch (command) {
-        case "concert-this": getMyBands(search);
+        case "concert-this": getBands(search);
             break;
-        case "spotify-this-song": getMeSpotify(search);
+        case "spotify-this-song": getSpotify(search);
             break;
         case "movie-this": getMovie(search);
             break;
@@ -27,7 +54,7 @@ var startProgram = function (command, search) {
 
 startProgram(command, search);
 
-function getMeSpotify(songName) {
+function getSpotify(songName) {
     if (songName == undefined) {
         songName = "Amarillo By Morning"
     }
@@ -41,15 +68,15 @@ function getMeSpotify(songName) {
             if (err) {
                 return console.log(err);
             }
-var songs  = data.tracks.items
-//console.log(JSON.stringify(songs))
-for(var i=0; i<songs.length; i++){
-    console.log("-----------------");
-    console.log("artist name:" +songs[i].artists[0].name);
-    console.log("song name: " +songs[i].name);
-    console.log("preview link:" +songs[i].preview_url);
-    console.log("album: " +songs[i].album.name);
-}
+            var songs = data.tracks.items
+            //console.log(JSON.stringify(songs))
+            for (var i = 0; i < songs.length; i++) {
+                console.log("-----------------");
+                console.log("artist name:" + songs[i].artists[0].name);
+                console.log("song name: " + songs[i].name);
+                console.log("preview link:" + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+            }
         });
 
 }
